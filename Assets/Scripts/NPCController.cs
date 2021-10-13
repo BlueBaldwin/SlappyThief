fc4f666b6b11ba355c14d2bd027429a91c805ee5
Scott Baldwin <71764565+ScottBaldwin0@users.noreply.github.com>
2021-10-14 00:34:32 +0100

    Too tired to care

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -3,13 +3,56 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = System.Random;
 
 [RequireComponent(typeof(NavMeshAgent))]
 
 public class NPCController : MonoBehaviour
 {
-   [SerializeField] static List<GameObject> shopPos = new List<GameObject>();
 
-   public int arrlengtrh = shopPos.Count;
+   public GameObject[] shoppingPoints = null;
 
+   // If timer reaches 0 then messMade becomes true and move on to next point
+   private NavMeshAgent agent;
+   private Vector3 destination;
+   private float messTimer = 3.0f;
+   private bool moveTarget = false;
+   private GameObject target = null;
+   private int randomPoint;
+
+
+   private void Start()
+   {
+      int arrayLength = shoppingPoints.Length;
+      agent = GetComponent<NavMeshAgent>();
+   }
+
+   private void Update()
+   {
+
+      if (!moveTarget)
+      {
+         messTimer -= (1 * Time.deltaTime);
+         if (messTimer <= 0)
+         {
+            ChangePos();
+         }
+      }
+   }
+
+// Random number (point) chosen 
+   void ChangePos()
+   {
+      randomPoint = UnityEngine.Random.Range(1, shoppingPoints.Length);
+      target = shoppingPoints[randomPoint];
+      print(target);
+      print(target.transform.position);
+      destination = target.transform.position;
+      agent.destination = destination;
+      moveTarget = false;
+   }
 }
+// Pass that number to move character
+// Once character arrives - count down 3's
+// Change sprite
+// repeat
