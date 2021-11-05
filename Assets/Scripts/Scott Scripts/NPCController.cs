f0a26c3b86d6504deab6f5386158afd289280a01
Scott Baldwin <71764565+ScottBaldwin0@users.noreply.github.com>
2021-11-05 15:32:19 +0000

    NPC Going to a single location
    Currently NPC arrives at a random location after 5 seconds.
    Will need to next check for arrival of NPC to that location to reset the timer and change location.
    
    Perhaps later down the line we could set it so the timer will also be random, meaning each NPC will stay at one shopping location between say 5 - 10 seconds at a time, to avoid sync movement?

--- Assets/Scripts/Scott Scripts/NPCController.cs
+++ Assets/Scripts/Scott Scripts/NPCController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
@@ -17,9 +18,11 @@ public class NPCController : MonoBehaviour
     private NavMeshAgent navMeshAgent = null;
     private Rigidbody rb = null;
 
-    private bool movePosition = false;
+    private bool movePosition = true;
+    private bool startTimer = false;
     private int totalWaypoints;
     private int randomPosition;
+    private float timer = 5f;
     
     void Awake()
     {
@@ -31,19 +34,45 @@ public class NPCController : MonoBehaviour
 
     private void Update()
     {
+        if (startTimer == false && timer > 0)           // Need timer to start once arrived at pos
+        {
+            StartCoroutine(ShoppingTimer());
+        }
         if (movePosition)
         {
            SetDestination();
         }
         
-    }
+        Debug.Log(currentTarget);
 
+    }
+    // Setting a new Destination initially and once called
     private void SetDestination()
     {
         if (navMeshAgent != null)
         {
             randomPosition = Random.Range(1, totalWaypoints);
-            navMeshAgent.destination = currentTarget.position;
+            currentTarget = waypoints[randomPosition];
+            movePosition = false;
+            return;
         }
+    }
+    // Initiating the NPC's Movement to the destination
+    private void Move()
+    {
+        navMeshAgent.destination = currentTarget.position;
+    }
+
+    IEnumerator ShoppingTimer()
+    {
+        startTimer = true;
+        yield return new WaitForSeconds(1);
+        timer -= 1;
+        startTimer = false; // Needs resting once NPC arrives at the location set
+        if (startTimer == false && timer <= 0)
+        {
+            Move();
+        }
+        
     }
 }
