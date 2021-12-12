08d064f038c97aeefca5767e2bc01b9b18b0499c
Scott Baldwin <71764565+BlueBaldwin@users.noreply.github.com>
2021-12-12 20:34:22 +0000

    Menu / Instructions screen buttons working

---
+++ Assets/Scripts/Scott Scripts/SceneIndex.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneIndex : MonoBehaviour
+{
+    private int sceneIndex;
+
+    void start()
+    {
+        //sceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+     public void StartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
