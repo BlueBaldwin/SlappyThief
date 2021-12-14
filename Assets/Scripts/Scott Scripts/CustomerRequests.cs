ea7cb9520de54030b3c4d12a37c327935ef22109
Scott Baldwin <S4010770@glos.ac.uk>
2021-12-14 00:24:03 +0000

    I hope this doesn't break Used a different project version!!

---
+++ Assets/Scripts/Scott Scripts/CustomerRequests.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerRequests : MonoBehaviour
+{
+    [SerializeField] private GameObject thoughtBubble = null;
+
+    private void Start()
+    {
+        NPCManager.inGameNPC[0].
+    }
+}
--- Assets/Scripts/Zac Scripts/ShopScripts/ShopItem.cs
+++ Assets/Scripts/Zac Scripts/ShopScripts/ShopItem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,15 @@ using Leap.Unity.Interaction;
 
 public class ShopItem : MonoBehaviour
 {
+    public enum ShopItemType 
+    { 
+        RED_SHIRT = 0,
+        BLUE_SHIRT = 1,
+        TROUSERS = 2,
+    }
+
+    [SerializeField] private ShopItemType Type;
+        
     [SerializeField]
     public Sprite PreviewSprite; //need a sprite for speech/thought bubble?
 
