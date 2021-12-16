92a1fe958392a1ea70ab28018a853e41e18f2562
s1703955 <s1703955@glos.ac.uk>
2021-12-16 02:02:51 +0000

    end screen fix
    cool

--- Assets/Scripts/Zac Scripts/UIScripts/EndScreen.cs
+++ Assets/Scripts/Zac Scripts/UIScripts/EndScreen.cs
@@ -7,21 +7,31 @@ public class EndScreen : MonoBehaviour
 {
 
     [SerializeField]
-    List<Sprite> Sprites;
+    List<Sprite> StampSprites;
 
-    List<float> Scores;
+    [SerializeField]
+    List<Sprite> BoxSprites;
     [SerializeField]
     List<Image> Images;
 
-    void Init()
+    [SerializeField]
+    Image BigStamp;
+    void Awake()
     {
+        List<float> Scores = new List<float>();
         Scores.Add(ScoreStatics.tidinessScore);
         Scores.Add(ScoreStatics.securityScore);
         Scores.Add(ScoreStatics.serviceScore);
+
         for(int i = 0; i < Scores.Count; ++i)
-        {
-            Images[i].sprite = Scores[i] < 20 ? Sprites[0] : Sprites[(int)Scores[i]/20]; // this is either the best or worst code ive ever written im so tired ive slept 3 of the last 48 hrs
+        {      
+            Images[i].enabled = true;     
+            Images[i].sprite = Scores[i] < 40 ? BoxSprites[0] : BoxSprites[1];
+            Images[i].preserveAspect = true;
         }
+
+         BigStamp.sprite = ScoreStatics.totalScore < 60 ? StampSprites[0] : StampSprites[(int)ScoreStatics.totalScore/60]; // this is either the best or worst code ive ever written im so tired ive slept 3 of the last 48 hrs
+        BigStamp.preserveAspect = true; 
     }
 }
 
